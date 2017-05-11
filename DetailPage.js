6455dbacb2bc0c8f3a09a60f49ed512db9f9f1ee
dmytrokendzerskyi <dmytrokendzerskyi@gmail.com>
2017-05-11 16:49:02 +0300

    detail page back

--- DetailPage.js
+++ DetailPage.js
@@ -6,24 +6,36 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
-
+  Image,
+  Button,
+  Navigator,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
 class DetailPage extends React.Component{
+  goBack(){
+    this.props.navigator.pop({
+      id: 'StartPage'
+    });
+  }
   render(){
     return (
-
-
         <View style={styles.background}>
+          <ScrollView>
+          <View style={styles.backImage}> 
+          <TouchableOpacity onPress={(this.goBack.bind(this))}>
+        <Image style={{width:30 , height:30}} source={require('./image/ic_arrow.png')}/>
+         </TouchableOpacity>
+        </View>
            <Image style={styles.imageEvent} source={{uri: this.props.data.image_event}}/>
            <View>
               <Text style={styles.titleText}>{ this.props.data.name_event}</Text>
-           </View>
+           </View> 
            <Text style = {styles.textEvent}>{this.props.data.description_event}</Text>
            <Text style = {styles.textEvent}>{this.props.data.date_event}</Text>
            <Text style = {styles.textEvent}>{this.props.data.name_company}</Text>
-
+           </ScrollView>
         </View>
     );
 
@@ -40,10 +52,19 @@ const styles = StyleSheet.create({
    titleText: {
     paddingTop:10,
     paddingBottom:10,
-    color: 'white',
+    color: 'white',    
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  backImage:{
+    alignSelf : 'stretch',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    flex:0,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)'
+ 
   },
   welcome: {
     fontSize: 20,
@@ -56,7 +77,8 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   imageEvent: {
-    height:500
+    height:500,
+    resizeMode: 'cover'
   },
  textEvent : {
     flex:1,
